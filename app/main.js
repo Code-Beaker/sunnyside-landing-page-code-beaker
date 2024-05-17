60bd577a74d72944da0fc7297de3d0e5158d459a
Tharun Raj <tharun.2.linux@gmail.com>
2024-05-17 10:53:01 +0530

    add mobile navigation toggle and navigation

--- app/main.js
+++ app/main.js
@@ -1,1 +1,9 @@
 import "./css/style.css";
+
+const navToggleButton = document.getElementById("navToggleButton");
+const navLinks = document.querySelector(".nav__links");
+
+navToggleButton.addEventListener("click", () => {
+  navToggleButton.classList.toggle("nav__toggle-btn-focus");
+  navLinks.classList.toggle("nav__links-show");
+});
